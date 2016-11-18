9e45262140ff5d7a536b5c82f25773581a009873
Toni Heikkinen <Toni Heikkinen>
2016-11-18 16:26:22 +0200

    default encoding to utf-8

--- ttepo/wsgi.py
+++ ttepo/wsgi.py
@@ -6,7 +6,8 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
-
+reload(sys)     
+sys.setdefaultencoding("utf-8")
 import os
 
 from django.core.wsgi import get_wsgi_application
