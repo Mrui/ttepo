3d3208765c90f635baed1bd6817077cf28c43c46
Toni Heikkinen <Toni Heikkinen>
2016-11-18 16:52:32 +0200

    wsgi.py update

--- ttepo/wsgi.py
+++ ttepo/wsgi.py
@@ -6,8 +6,7 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
-reload(sys)     
-sys.setdefaultencoding("utf-8")
+
 import os
 
 from django.core.wsgi import get_wsgi_application
